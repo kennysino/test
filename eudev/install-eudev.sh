2ccd18745ccfec052eb3c049b3324cde522cadfa
Ing <wjz304@qq.com>
2022-08-25 00:02:23 +0800

    add eudev

---
+++ eudev/install-eudev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env ash
+
+
+tar -zxvf eudev.tar.gz -C /tmpRoot/
+
+echo "Starting eudev daemon"
+[ -e /proc/sys/kernel/hotplug ] && printf '\000\000\000\000' > /proc/sys/kernel/hotplug
+/sbin/udevd -d || { echo "FAIL"; exit 1; }
+echo "Triggering add events to udev"
+udevadm trigger --type=subsystems --action=add
+udevadm trigger --type=devices --action=add
+udevadm settle --timeout=10 || echo "udevadm settle failed"
+
